54b0726b236b98be9f8a36317fcf1ede9fb551f6
singhneetush <singh.neetush@gmail.com>
2023-05-30 10:20:13 +0530

    map-handling

--- components/Map.js
+++ components/Map.js
@@ -1,44 +1,49 @@
-import { useEffect, useContext } from 'react'
-import mapboxgl from 'mapbox-gl'
-import { UberContext } from '../context/uberContext'
+import { useEffect, useContext } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { UberContext } from '../context/uberContext';
 
 const style = {
-  wrapper: `flex-1 h-full w-full`,
-}
+	wrapper: `flex-1 h-full w-full`,
+};
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+// mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+mapboxgl.accessToken = "pk.eyJ1IjoibmVldHVzaW5naCIsImEiOiJjbGk5cjNuaHMwZDNhM2ZwYjM1cjJoejhpIn0.2Payr8HqDYAJ1m_dLClt6Q";
+
+
+
+console.log('hello : ');
 
 const Map = () => {
-  const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
+	const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext);
 
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
-      center: [-99.29011, 39.39172],
-      zoom: 3,
-    })
+	useEffect(() => {
+		const map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+			center: [-99.29011, 39.39172],
+			zoom: 3,
+		});
 
-    if (pickupCoordinates) {
-      addToMap(map, pickupCoordinates)
-    }
+		if (pickupCoordinates) {
+			addToMap(map, pickupCoordinates);
+		}
 
-    if (dropoffCoordinates) {
-      addToMap(map, dropoffCoordinates)
-    }
+		if (dropoffCoordinates) {
+			addToMap(map, dropoffCoordinates);
+		}
 
-    if (pickupCoordinates && dropoffCoordinates) {
-      map.fitBounds([dropoffCoordinates, pickupCoordinates], {
-        padding: 400,
-      })
-    }
-  }, [pickupCoordinates, dropoffCoordinates])
+		if (pickupCoordinates && dropoffCoordinates) {
+			map.fitBounds([dropoffCoordinates, pickupCoordinates], {
+				padding: 400,
+			});
+		}
+	}, [pickupCoordinates, dropoffCoordinates]);
 
-  const addToMap = (map, coordinates) => {
-    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
-  }
+	const addToMap = (map, coordinates) => {
+		const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+	};
 
-  return <div className={style.wrapper} id='map' />
-}
+	return <div className={style.wrapper} id='map' />;
+};
 
-export default Map
+export default Map;
